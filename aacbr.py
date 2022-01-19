c5760ce34983acfaaa97093fef6d37d2a77726ad
Guilherme Passos <gpp_92@hotmail.com>
2022-01-19 14:30:56 -0300

    first modifications to main files

--- aacbr.py
+++ aacbr.py
@@ -1,49 +1,6 @@
-#!/usr/bin/env python3
-# -*- encoding: utf-8 -*-
+### This encapsulates AA-CBR as a model.
 
-import sys
-import os
+## We try to have it compatible with the scikit-learn API:
+## https://scikit-learn.org/stable/developers/develop.html#apis-of-scikit-learn-objects
 
-from datetime import datetime
-from datetime import timedelta
-import time
-
-import json
-
-from cases import loadCases, giveCasebase, giveNewcases
-from predictions import givePredictions
-
-from variables import *
-
-
-def interact(cases_filename: str, newcases_filename: str):
-	'''Asks the user to provide files with cases and new cases
-		and outputs AA-CBR predictions in .json format'''
-
-	print('{now}: Started'.format(now = datetime.now().strftime('%Y-%m-%d %H.%M.%S')))
-	cases_file = os.path.join(os.getcwd(), '{}.json'.format(cases_filename))
-	if not os.path.isfile(cases_file):
-		return print('Casebase file {} not found. Try again.\n'.format(cases_filename))	
-	else:			
-		cases = loadCases(cases_file)
-		casebase = giveCasebase(cases) 	
-	newcases_file = os.path.join(os.getcwd(), '{}.json'.format(newcases_filename))
-	if not os.path.isfile(newcases_file):
-		return print('New cases file {} not found. Try again.\n'.format(newcases_filename))	
-	else:
-		new_cases = loadCases(newcases_file)
-		newcases = giveNewcases(casebase, new_cases)
-	
-	Predictions = givePredictions(casebase, newcases)
-
-	predictions_output_filename = '{c}_to_{n}.json'.format(c = cases_filename, n = newcases_filename)
-	with open(os.path.join(os.getcwd(), predictions_output_filename), 'w', newline = '', encoding = 'utf-8') as output:
-		json.dump(Predictions, output, indent = 4, ensure_ascii = False)
-	output.close()
-		
-	return print('{now}: Done. Predictions dumped to {file}'.format(now = datetime.now().strftime('%Y-%m-%d %H.%M.%S'), file = predictions_output_filename))
-	
-
-if __name__ == "__main__":
-	interact('cb', 'new')
 
--- cases.py
+++ cases.py
@@ -1,100 +1,110 @@
 
 import json
 
-from variables import *
+from .variables import *
 
 
 class Case:
-	'''Defines a case to comprise id, factors, outcome, 
-		lists of attackees and attackers and a label'''
+  '''Defines a case to comprise id, factors, outcome, 
+    lists of attackees and attackers and a label'''
 
-	def __init__(self, id, factors, outcome, attackees: list, attackers: list, label: int):
-		self.id = id
-		self.factors = factors
-		self.outcome = outcome
-		self.attackees = attackees
-		self.attackers = attackers
-		self.label = label
-
-		
+    def __init__(self, id, factors, outcome=None, attackees=None, attackers=None, label=None, weight=None):
+        self.id = id
+        self.factors = factors
+        self.outcome = outcome
+        self.attackees = attackees if attackees else []
+        self.attackers = attackers if attackers else []
+        self.label = label
+        if weight:
+            self.weight = weight
+    def __str__(self):
+        return f'Case("id": {self.id}, "factors": {self.factors}, "outcome": {self.outcome}, "attackees": {[attacker.id for attacker in self.attackees]})'
+    def __repr__(self):
+        return f'Case("id": {self.id}, "factors": {self.factors}, "outcome": {self.outcome}, "attackees": {[attacker.id for attacker in self.attackees]})'
+    def __eq__(self, other):
+        if not isinstance(other, Case):
+            return NotImplemented
+        return all([self.id == other.id, set(self.factors) == set(other.factors), self.outcome == other.outcome])
+    def __hash__(self):
+        return hash((self.id, frozenset(self.factors), self.outcome))
+    
 def differentOutcomes(A, B):
-	return A.outcome != B.outcome
+  return A.outcome != B.outcome
 
 
 def moreSpecific(A, B):
-	return B.factors.issubset(A.factors) and B.factors != A.factors
+  return B.factors.issubset(A.factors) and B.factors != A.factors
 
 
 def mostConcise(cases, A, B):
-	return not any((moreSpecific(A, case) and moreSpecific(case, B) and not(differentOutcomes(A, case))) for case in cases)
+  return not any((moreSpecific(A, case) and moreSpecific(case, B) and not(differentOutcomes(A, case))) for case in cases)
 
 
 def attacks(cases, A, B):
-	return differentOutcomes(A, B) and moreSpecific(A, B) and mostConcise(cases, A, B)
+  return differentOutcomes(A, B) and moreSpecific(A, B) and mostConcise(cases, A, B)
 
 
 def newcaseattacks(newcase, targetcase):
-	return not newcase.factors.issuperset(targetcase.factors)
-	
-	
+  return not newcase.factors.issuperset(targetcase.factors)
+  
+  
 def inconsistentattacks(A, B):
-	return differentOutcomes(A, B) and B.factors == A.factors
+  return differentOutcomes(A, B) and B.factors == A.factors
 
 
-def loadCases(file: str) -> list:
-	'''Loads cases form a file'''
+def load_cases(file: str) -> list:
+  '''Loads cases form a file'''
 
-	global ID_DEFAULT, OUTCOME_DEFAULT, ID_NON_DEFAULT, OUTCOME_NON_DEFAULT
+  global ID_DEFAULT, OUTCOME_DEFAULT, ID_NON_DEFAULT, OUTCOME_NON_DEFAULT
 
-	cases = []
-	with open(file, encoding = 'utf-8') as json_file:
-		entries = json.load(json_file)
-		for entry in entries:
-			if entry['id'] == ID_DEFAULT:
-				default_case = Case(ID_DEFAULT, set(), OUTCOME_DEFAULT, [], [], 0)
-				cases.insert(0, default_case)
-				# uncomment the following 2 lines for two defaults
-#				non_default_case = Case(ID_NON_DEFAULT, set(), OUTCOME_NON_DEFAULT, [], [], 0)
-#				cases.insert(0, non_default_case)
-			else:
-				case = Case(entry['id'], set(entry['factors']), entry['outcome'], [], [], 0)
-				cases.append(case)
-	json_file.close()
-	
-	return cases
-	
-	
-def giveCasebase(cases: list) -> list:
-	'''Returns a casebase given a list of cases'''
+  cases = []
+  with open(file, encoding = 'utf-8') as json_file:
+    entries = json.load(json_file)
+    for entry in entries:
+      if entry['id'] == ID_DEFAULT:
+        default_case = Case(ID_DEFAULT, set(), OUTCOME_DEFAULT, [], [], 0)
+        cases.insert(0, default_case)
+        # uncomment the following 2 lines for two defaults
+#        non_default_case = Case(ID_NON_DEFAULT, set(), OUTCOME_NON_DEFAULT, [], [], 0)
+#        cases.insert(0, non_default_case)
+      else:
+        case = Case(entry['id'], set(entry['factors']), entry['outcome'], [], [], 0)
+        cases.append(case)
+  json_file.close()
+  
+  return cases
+  
+  
+def give_casebase(cases: list) -> list:
+  '''Returns a casebase given a list of cases'''
 
-	casebase = []
-	for candidate_case in cases:
-		duplicate = False
-		for case in casebase:
-			if candidate_case.id != case.id and candidate_case.factors == case.factors and candidate_case.outcome == case.outcome:
-				duplicate = True
-		if not duplicate:
-			casebase.append(candidate_case)	
-	
-	for case in casebase:
-		for othercase in casebase:
-			if attacks(casebase, case, othercase) or inconsistentattacks(case, othercase):
-				case.attackees.append(othercase)
-				othercase.attackers.append(case)
-				
-	return casebase
+  casebase = []
+  for candidate_case in cases:
+    duplicate = False
+    for case in casebase:
+      if candidate_case.id != case.id and candidate_case.factors == case.factors and candidate_case.outcome == case.outcome:
+        duplicate = True
+    if not duplicate:
+      casebase.append(candidate_case)  
+  
+  for case in casebase:
+    for othercase in casebase:
+      if attacks(casebase, case, othercase) or inconsistentattacks(case, othercase):
+        case.attackees.append(othercase)
+        othercase.attackers.append(case)
+        
+  return casebase
 
 
-def giveNewcases(casebase: list, cases: list) -> list:
-	'''Returns a a list of new cases given a list of cases and a casebase'''
+def give_newcases(casebase: list, cases: list) -> list:
+  '''Returns a a list of new cases given a list of cases and a casebase'''
 
-	newcases = []
-	for newcase in cases:
-		for case in casebase:
-			if newcaseattacks(newcase, case):
-				newcase.attackees.append(case)
-		newcases.append(newcase)
-		
-	return newcases
-	
-	
+  newcases = []
+  for newcase in cases:
+    for case in casebase:
+      if newcaseattacks(newcase, case):
+        newcase.attackees.append(case)
+    newcases.append(newcase)
+    
+  return newcases
+
