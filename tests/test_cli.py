5e667456adff9923cc1a14e4cd195ead60d0ba21
Guilherme Passos <gpp_92@hotmail.com>
2022-01-24 18:47:36 -0300

    adapting cli test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,33 +1,45 @@
 ### Testing command line interface
 
+## currently this works for 'main', the original implementation
+
 import pytest
 import os
 from shutil import copy
+from pathlib import Path
+import subprocess
 
+# pytest_plugins = ["pytester"]
 
-CLI_FILE="aacbr/cli.py"
+CLI_FILE="cli.py"
 
 @pytest.fixture
-def run(pytester):
+def run():
   original_dir = os.getcwd()
+  # raise(Exception(original_dir))
+  # raise(Exception([child for child in Path(original_dir).iterdir()]))
   def do_run(*args):
-    args = [f"{original_dir}/python", CLI_FILE] + list(args)
-    return pytester._run(*args)
+    args = ["python", f"{original_dir}/"+CLI_FILE] + list(args)
+    return subprocess.run(args, capture_output=True)
   return do_run
 
 def test_run_aacbr_cli_default_arguments(tmp_path, run):
   data_files = ["cb.json", "new_5.json", "cb_to_new_5.json"]
-  os.chdir(tmp_path)
-  
-  for f in data_files:
-    copy(f"data/{f}", f"{f}")  
-  
-  with open("cb_to_new_5.json") as f:
+  data_dir = "../tests/data"
+
+  copy(Path(f"{data_dir}/cb.json"), f"{tmp_path}/cb.json")
+  copy(Path(f"{data_dir}/new_5.json"), f"{tmp_path}/new.json")
+  # copy(Path(f"{data_dir}/cb_to_new_5.json"), f"{tmp_path}/cb_to_new.json")
+
+  with open(f"{data_dir}/cb_to_new_5.json") as f:
     expected = f.read()
-  tmp_path.unlink("cb_to_new_5.json") # to make sure it is now empty
+  
+  # raise(Exception(tmp_path))
+  # raise(Exception([child for child in Path(tmp_path).iterdir()]))
+  os.chdir(tmp_path)
 
   result = run()
-  with open("cb_to_new_5.json") as f:
-    # run() changes this file
+  # raise(Exception(result.stdout, result.stderr))
+  assert Path("cb_to_new.json").exists(), f"{CLI_FILE} is not generating output files."
+  with open("cb_to_new.json") as f:
     result = f.read()
   assert result == expected
