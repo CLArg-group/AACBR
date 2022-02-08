8a762fe8bf65100a49522abb74b167071b346efe
Guilherme Passos <gpp_92@hotmail.com>
2022-02-08 01:46:02 -0300

    setting up dependencies in setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,11 @@ import setuptools
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
+
+requires = [
+    "matplotlib==3.5.1",
+    "networkx==2.6.3"
+    ]
     
 setuptools.setup(
     name="aacbr",
@@ -22,5 +27,6 @@ setuptools.setup(
     ],    
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
-    python_requires=">=3.9"
+    python_requires=">=3.9",
+    install_requires=requires
 )
